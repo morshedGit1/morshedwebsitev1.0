5273c1a994ee70a6d823353da99ada462ad3b81c
Abdulmajeed Allayati <Gamer_Killer@abdulmajeedsair.kfupm.edu.sa>
2017-12-16 00:26:01 +0300

    Adding all terms & condition

--- js/coustom.js
+++ js/coustom.js
@@ -1,27 +1,89 @@
-// Get the modal
-var modal = document.getElementById('myModal');
-var modalContent = document.getElementById('modal-content');
-// Get the button that opens the modal
-var btn = document.getElementById("btn");
+function cancelationTerms() {
+  // Get the modal
+  var modal = document.getElementById('cancelationTermsModal');
+  var modalContent = document.getElementById('modal-content');
+  // Get the button that opens the modal
+  var btn = document.getElementById("cancelationTerms");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-    modalContent.className += " animated fadeIn";
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+      modal.style.display = "block";
+      modalContent.className += " animated fadeIn";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+      modalContent.className = " modal-content";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-    modalContent.className = " modal-content";
+function travellerTermsAndCondition() {
+  // Get the modal
+  var modal = document.getElementById('travellerTermsAndConditionModal');
+  var modalContent = document.getElementById('modal-content');
+  // Get the button that opens the modal
+  var btn = document.getElementById("travellerTermsAndCondition");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+      modal.style.display = "block";
+      modalContent.className += " animated fadeIn";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+      modalContent.className = " modal-content";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function privacyAndPolicy() {
+  // Get the modal
+  var modal = document.getElementById('privacyAndPolicyModal');
+  var modalContent = document.getElementById('modal-content');
+  // Get the button that opens the modal
+  var btn = document.getElementById("privacyAndPolicy");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+      modal.style.display = "block";
+      modalContent.className += " animated fadeIn";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+      modalContent.className = " modal-content";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
 }
