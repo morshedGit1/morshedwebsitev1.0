9f98ae4e478848c171e5846c5f3a48c5cd20d997
Abdulmajeed Allayati <Gamer_Killer@abdulmajeedsair.kfupm.edu.sa>
2017-12-19 23:36:59 +0300

    Adding php code

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php include_once("home.html") ?>
+<?php
+
+  include_once("home.html");
+
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $to = "hello@morshedapp.com";
+  $subject = "New Request";
+
+  mail($to, $subject, $message, "From: " . $name);
+
+?>
