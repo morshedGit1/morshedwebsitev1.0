b5166ee890ffcbd5435ed4237e42174e5a82c111
Abdulmajeed Allayati <Gamer_Killer@abdulmajeedsair.kfupm.edu.sa>
2017-12-20 23:07:46 +0300

    Edit php file

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
-
   include_once("home.html");
-
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  $to = "hello@morshedapp.com";
-  $subject = "New Request";
-
-  mail($to, $subject, $message, "From: " . $name);
-  echo "facebook";
 ?>
